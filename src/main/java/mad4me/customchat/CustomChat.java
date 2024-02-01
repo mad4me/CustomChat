2826f195336e01d0278353a0d7f04b94a2853d3a
Mad4Me <srayroman9@gmail.com>
2024-02-01 22:40:28 +0000

    Initiated project

---
+++ src/main/java/mad4me/customchat/CustomChat.java
@@ -0,0 +1,17 @@
+package mad4me.customchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
